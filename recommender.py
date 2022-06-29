8312b6752868f5b3e579ef53fb39a152ffd01e73
FluxusCrow <pascal.kroh@web.de>
2022-06-29 11:31:15 +0200

    removed pandas import

--- recommender.py
+++ recommender.py
@@ -2,9 +2,6 @@
 dummy recommender recommending random movies
 '''
 
-import pandas as pd
-
-
 def recommend_random(movies, k=10):
     """
     Dummy recommender that recommends a list of random movies. Ignores the actual query.
